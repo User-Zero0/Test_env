029898a4ba214f9ca0123ef730e29a7038cf5499
Ildus-91 <ildus91ov@gmail.com>
2024-07-19 11:59:33 +0400

    base_config_setup

---
+++ src/core/config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class RunConfig(BaseSettings):
+    host: str = "0.0.0.0"
+    port: int = 8002
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class Settings(BaseSettings):
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+
+
+settings = Settings()
---
+++ src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+from core.config import settings
+from api import router as api_router
+
+
+app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix,
+)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
