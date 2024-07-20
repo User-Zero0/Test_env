4a9b3d4bf89a12850fe28793e63d3a051224354e
Ildus-91 <ildus91ov@gmail.com>
2024-07-20 19:38:52 +0400

    Dataase_orm_setup

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,10 @@
+# src/core/config.py
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
 
 
 class RunConfig(BaseSettings):
@@ -7,13 +12,34 @@ class RunConfig(BaseSettings):
     port: int = 8002
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),  # .env.template
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
+
+# Debugging print statements
+print(f"Run configuration: {settings.run}")
+print(f"API configuration: {settings.api}")
+print(f"Database configuration: {settings.db}")
+print(f"Database URL: {settings.db.url}")
--- src/main.py
+++ src/main.py
@@ -1,15 +1,32 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
 
 from core.config import settings
 from api import router as api_router
+from core.models import db_helper
 
 
-app = FastAPI()
-app.include_router(
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup process
+    yield
+    # shutdown
+    print("dispose_engine")
+    await db_helper.dispose()
+
+
+main_app = FastAPI(
+    lifespan=lifespan,
+)
+
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
+    uvicorn.run(
+        "main:main_app", host=settings.run.host, port=settings.run.port, reload=True
+    )
